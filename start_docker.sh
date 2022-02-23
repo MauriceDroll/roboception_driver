2aaba84692d38ffdc0d56bd89c2fc6679c085680
FrederikPlahl <plfr1013@h-ka.de>
2022-02-23 11:50:34 +0100

    Changed uid and gid

--- start_docker.sh
+++ start_docker.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-# uid=$(eval "id -u")
-# gid=$(eval "id -g")
+uid=$(eval "id -u")
+gid=$(eval "id -g")
 
 # Mac user:
-uid=1000
-gid=1000
+# uid=1000
+# gid=1000
 
 docker build --build-arg UID="$uid" --build-arg GID="$gid" -t roboception_driver/ros:foxy .
 echo "Run Container"
